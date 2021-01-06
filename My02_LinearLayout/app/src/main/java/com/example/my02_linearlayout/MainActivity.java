1926334be67cd7193b496ea4d1939af5eeac8555
302-11\hanul <tpfk373@hanmail.net>
2021-01-06 11:43:14 +0900

    주석 추가

--- My02_LinearLayout/app/src/main/java/com/example/my02_linearlayout/MainActivity.java
+++ My02_LinearLayout/app/src/main/java/com/example/my02_linearlayout/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //안녕
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
